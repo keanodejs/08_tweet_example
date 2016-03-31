4366c81e1624c751f5f65b962855a800b45c93b8
Claus Bové <clbo@kea.dk>
2016-03-31 22:16:14 +0200

    initial

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var cfg = require('./config.json');
 var tw = require('node-tweet-stream')(cfg);
 tw.track('javascript');
 tw.on('tweet', function(tweet){
-  //io.emit('tweet', tweet);
-  console.log(JSON.stringify(tweet.text, null, 4));
+  
+  console.log(tweet);
 })
